c4de2b4f5a522e3ab7fed85888578dabc42087e8
unknown <vcosta1.dev@gmail.com>
2023-11-14 14:51:28 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,27 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha1 = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+
+// contatenação (+)
+
+// console.log(citacao + meuNome)
+
+// template string OU template literal
+
+const cidade = "belo Horizonte";
+const input = "Belo Horizonte";
+
+// console.log(cidade === input); // false
+
+const inputMinusculo = input.toLowerCase();
+
+// console.log(cidade === inputMinusculo); // true
+
+
+const senha = "victor";
+console.log(senha.length) // 13 caractere
