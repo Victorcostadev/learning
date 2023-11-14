0ae2dcc20e9fd96e0d66e29dd46ea75571c860cc
unknown <vcosta1.dev@gmail.com>
2023-11-14 16:20:24 -0300

    codigos var e conversoes

---
+++ exemplo.js
@@ -0,0 +1,24 @@
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+// console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+//let input = null;
+
+//if (input == null) {
+   // console.log('não há informação');
+//}
+//else {
+   // console.log(input);
+//}
+
+let input = null;
+let input2;
+
+console.log(input);
+console.log(input2);
+
+console.log(null == undefined);
+console.log(null === undefined);
